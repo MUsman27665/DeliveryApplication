3ca8bf72295994768729d212dddc33cf70813f46
MuhammadUsman <malik012.um@gmail.com>
2020-11-03 13:02:44 +0500

    secret key removed

--- iCV_Nation/settings.py
+++ iCV_Nation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a19iuqe=z4uyq%g+%8hn$i%flq*qz0j9z6c1+c@%m_c+vntgcz'
+#SECRET_KEY = 'a19iuqe=z4uyq%g+%8hn$i%flq*qz0j9z6c1+c@%m_c+vntgcz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
